7e7fb20eb4f2cc9c9a51bdcfed1baf2e5fb219af
seongkyu-lim <96x60812@gmail.com>
2022-12-09 12:44:31 +0900

    initializer

---
+++ src/main/java/com/alan/springexercisebook/SpringExerciseBookApplication.java
@@ -0,0 +1,13 @@
+package com.alan.springexercisebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExerciseBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExerciseBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alan/springexercisebook/SpringExerciseBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alan.springexercisebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExerciseBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

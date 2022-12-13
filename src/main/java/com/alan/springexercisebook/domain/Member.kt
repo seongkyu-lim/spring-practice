c7effb21bd0abfb9f27a9085ccd2a127c2f3ef50
seongkyu-lim <96x60812@gmail.com>
2022-12-13 11:46:18 +0900

    Feature: 코틀린, 자바 섞어서 사용해보기

---
+++ src/main/java/com/alan/springexercisebook/domain/Member.kt
@@ -0,0 +1,21 @@
+package com.alan.springexercisebook.domain
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import lombok.NoArgsConstructor
+import lombok.RequiredArgsConstructor
+
+@Entity
+@NoArgsConstructor
+public class Member (
+    var login: String,
+    var firstname: String,
+    var lastname: String,
+    var description: String? = null,
+    @Id @GeneratedValue var id: Long? = null){
+
+
+
+}
---
+++ src/main/java/com/alan/springexercisebook/service/MemberService.java
@@ -0,0 +1,20 @@
+package com.alan.springexercisebook.service;
+
+import com.alan.springexercisebook.domain.Member;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    public void createMember(){
+        Member member = new Member("test", "lim", "seongkyu", "테스트 멤버입니다.", 1L);
+        System.out.println(member.getDescription());
+    }
+}
+
+class Test{
+    public static void main(String[] args) {
+        MemberService memberService = new MemberService();
+        memberService.createMember();
+    }
+}

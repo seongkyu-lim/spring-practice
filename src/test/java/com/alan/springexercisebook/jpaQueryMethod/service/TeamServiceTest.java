44bba59561051921e0d7fe11856df0c5e648c5d1
seongkyu-lim <96x60812@gmail.com>
2022-12-21 20:06:10 +0900

    Test: container 만들어서 사용할 빈들만 주입해서 돌려보자

--- src/test/java/com/alan/springexercisebook/jpaQueryMethod/service/TeamServiceTest.java
+++ src/test/java/com/alan/springexercisebook/jpaQueryMethod/service/TeamServiceTest.java
@@ -18,6 +18,7 @@ class TeamServiceTest {
     @Test
     @DisplayName("매핑이 되어있는 팀 데이터 삭제해보기.")
     void removeTeam(){
+        // kotlin과 java 의  버전? 이 안맞아서 에러나는듯 ?
         Team team = teamService.addTeam("testTeam", 1);
         accountService.addAccount("test@gmail.com", "test", "test1234", team);
         accountService.addAccount("test2@gmail.com", "test2", "test1234", team);
